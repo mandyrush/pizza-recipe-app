5b78374f6ccf5c6109619b3e23c762518bd3409c
mandyrush <mandyrush85@gmail.com>
2021-10-10 19:12:46 -0600

    create initial project routes and controller

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,7 +1,27 @@
 const showProjects = (req, res) => {
-    console.log('List Projects Route');
+    console.log('Show all projects route.');
+}
+
+const showProject = (req, res) => {
+    console.log('Show single project by id route. ', req.params.id);
+}
+
+const createProject = (req, res) => {
+    console.log('Create new project route. ', req.body);
+}
+
+const updateProject = (req, res) => {
+    console.log('Update project by id route. ', req.body);
+}
+
+const deleteProject = (req, res) => {
+    console.log('Delete project by id route. ', req.params.id);
 }
 
 module.exports = {
-    showProjects
+    showProjects,
+    showProject,
+    createProject,
+    updateProject,
+    deleteProject
 }
--- routes/projects.js
+++ routes/projects.js
@@ -3,6 +3,19 @@ const router = express.Router();
 
 const controller = require('../controllers/projects');
 
+// Get all projects
 router.get('/projects', controller.showProjects);
+
+// Get a single project by it's id
+router.get('/projects/:id', controller.showProject);
+
+// Create a new project
+router.post('/project', controller.createProject);
+
+// Update a project by it's id
+router.put('/projects/:id', updateProject);
+
+// Delete a project by it's id
+router.delete('/projects/:id', deleteProject);
 
 module.exports = router;
