722d199031d3a562648db245d160b3261b485529
mandyrush <mandyrush85@gmail.com>
2022-01-01 18:40:14 -0700

    update show all ingredients controller to get recipe_id from query string

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -3,7 +3,7 @@ const db = require('../db/db');
 const showIngredients = (req, res) => {
     console.log('Show all ingredients from a recipe route.');
 
-    let recipe_id = req.body.recipe_id;
+    let recipe_id = req.query.recipe;
 
     let sql = 'SELECT * FROM ingredients INNER JOIN recipe_ingredient ON ingredients.id = recipe_ingredient.ingredient_id WHERE recipe_id = ?';
 
@@ -42,41 +42,41 @@ const createIngredient = (req, res) => {
     let sql = 'INSERT INTO ingredients (name) VALUES (?)';
 
     db.queryWrapper(sql, name)
-    .then (results => {
-        console.log('Got the insert id: ', results.insertId);
+        .then(results => {
+            console.log('Got the insert id: ', results.insertId);
 
-        let ingredient_id = results.insertId;
-        let recipe_id = req.body.recipe_id;
-        let quantity = req.body.quantity;
-        let notes = req.body.notes;
+            let ingredient_id = results.insertId;
+            let recipe_id = req.body.recipe_id;
+            let quantity = req.body.quantity;
+            let notes = req.body.notes;
 
-        let params = [];
-        params.push(ingredient_id);
-        params.push(recipe_id);
-        params.push(quantity);
-        params.push(notes);
+            let params = [];
+            params.push(ingredient_id);
+            params.push(recipe_id);
+            params.push(quantity);
+            params.push(notes);
 
-        let sql = 'INSERT INTO recipe_ingredient (ingredient_id, recipe_id, quantity, notes) VALUES (?, ?, ?, ?)';
+            let sql = 'INSERT INTO recipe_ingredient (ingredient_id, recipe_id, quantity, notes) VALUES (?, ?, ?, ?)';
 
-        db.queryWrapper(sql, params)
-        .then (
-            results => {
-                console.log(results);
-                res.sendStatus(204);
-            }  
-        )           
-    })
-    .catch (error => {
-        console.log(`Failed to create ingredient`, error);
-        res.sendStatus(500);
-    })  
+            db.queryWrapper(sql, params)
+                .then(
+                    results => {
+                        console.log(results);
+                        res.sendStatus(204);
+                    }
+                )
+        })
+        .catch(error => {
+            console.log(`Failed to create ingredient`, error);
+            res.sendStatus(500);
+        })
 
     // @todo - Figure out how to convert to async await
     // async function addIngredient() {
     //     let name = req.body.name;
     //     let addNamesql = 'INSERT INTO ingredients (name) VALUES (?)';
     //     let response = await db.queryWrapper(addNamesql, name);
-        
+
     //     if (!response.ok) {
     //         throw new Error('HTTP error! status: 503');
     //     }
@@ -118,29 +118,29 @@ const updateIngredient = (req, res) => {
     let sql = 'UPDATE ingredients SET name = ? WHERE id = ?';
 
     db.queryWrapper(sql, params)
-    .then (results => {
-        console.log(`Updating Ingredient with id: ${ingredient_id}`, results);
+        .then(results => {
+            console.log(`Updating Ingredient with id: ${ingredient_id}`, results);
 
-        let quantity = req.body.quantity;
-        let notes = req.body.notes;
+            let quantity = req.body.quantity;
+            let notes = req.body.notes;
 
-        let params = [];
-        params.push(quantity);
-        params.push(notes);
-        params.push(ingredient_id);
+            let params = [];
+            params.push(quantity);
+            params.push(notes);
+            params.push(ingredient_id);
 
-        let sql = 'UPDATE recipe_ingredient SET quantity = ?, notes = ? WHERE ingredient_id = ?';
+            let sql = 'UPDATE recipe_ingredient SET quantity = ?, notes = ? WHERE ingredient_id = ?';
 
-        db.queryWrapper(sql, params)
-        .then (results => {
-            console.log(results);
-            res.sendStatus(204);
-        })          
-    })
-    .catch (error => {
-        console.log(`Failed to create ingredient`, error);
-        res.sendStatus(500);
-    })
+            db.queryWrapper(sql, params)
+                .then(results => {
+                    console.log(results);
+                    res.sendStatus(204);
+                })
+        })
+        .catch(error => {
+            console.log(`Failed to create ingredient`, error);
+            res.sendStatus(500);
+        })
 }
 
 const deleteIngredient = (req, res) => {
